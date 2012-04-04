1e72167ad474058b6dc1ee4d37f75ed12fd251df
Casey Grzecka <c@sey.gr>
2012-04-04 15:10:49 -0700

    first commit

---
+++ drupal-frp.sh
@@ -0,0 +1,7 @@
+mkdir sites/default/files;
+cp sites/default/default.settings.php sites/default/settings.php;
+chmod 775 sites/default/files;
+chmod 775 sites/default/settings.php;
+
+mkdir sites/all/module/contrib;
+mkdir sites/all/module/custom;
