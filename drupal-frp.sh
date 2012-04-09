d30d69f7bf53a4f420dafedddb88a30259d727b5
Casey Grzecka <c@sey.gr>
2012-04-09 15:22:24 -0700

    fixed typo and added the creation of the private files directory

--- drupal-frp.sh
+++ drupal-frp.sh
@@ -3,5 +3,9 @@ cp sites/default/default.settings.php sites/default/settings.php;
 chmod 775 sites/default/files;
 chmod 775 sites/default/settings.php;
 
-mkdir sites/all/module/contrib;
-mkdir sites/all/module/custom;
+mkdir sites/default/private;
+chmod 770 sites/default/private;
+
+mkdir sites/all/modules;
+mkdir sites/all/modules/contrib;
+mkdir sites/all/modules/custom;
